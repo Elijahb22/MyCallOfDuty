131f6cc0865b93918e9b93cc94dad9f7fd1c60d0
elijahb22 <elibusick11@gmail.com>
2022-04-12 19:53:28 -0400

    create login component and install @apollo/client

---
+++ client/src/components/login.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const login = () => {
+    return(
+        <main className='flex-row justify-center mb-4'>
+        <div className=''>
+          <div className='card'>
+            <h4 className='card-header'>Login</h4>
+            <div className='card-body'>
+              <form>
+                <input
+                  className='form-input'
+                  placeholder='Username'
+                  name='username'
+                  type='username'
+                  id='user'
+                />
+                <input
+                  className='form-input'
+                  placeholder='Password'
+                  name='password'
+                  type='password'
+                  id='password'
+                />
+                <button className='' type='submit'>
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </main>
+    )
+};
+
+export default login;
