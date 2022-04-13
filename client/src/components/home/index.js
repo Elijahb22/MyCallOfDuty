89e40051101dfbfb6b095eceb1799cca3e00068e
elijahb22 <elibusick11@gmail.com>
2022-04-12 23:01:29 -0400

    fixed bug with home page

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -4,8 +4,8 @@ const home = () => {
     return(
     <main>
         <div className="flex-row justify-space-between">
-            <div className="col-12 mb-3">
-                <div>Loading...</div>
+            <div className="">
+                <div></div>
             </div>
         </div>
     </main>
