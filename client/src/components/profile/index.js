c55ddf969332f0c48be9c553bdc7141413c7e7c8
elijahb22 <elibusick11@gmail.com>
2022-04-13 08:38:26 -0400

    create profile page and added react select to make an array for options of rank

---
+++ client/src/components/profile/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Select from 'react-select';
+
+const Profile = () => {
+const rank = [
+    { label: "Contender", value: 1 },
+    { label: "Specialist", value: 2 },
+    { label: "Advance", value: 3 },
+    { label: "Expert", value: 4 },
+    { label: "Elite", value: 5 },
+    { label: "Master", value: 6 },
+    { label: "Challenger", value: 7 },
+    { label: "Top 250", value: 8 },
+    { label: " Warzone", value: 9 }
+]
+return (
+    <div>
+        <div className="">
+            <h2 className="">My  profile.</h2>
+            <Select placeholder="N/A" options={rank}></Select>
+            <button className="btn ">Add Friend</button>
+        </div>
+        <div className="flex-row justify-space-between">
+            <div className="">
+                Posts
+            </div>
+            <div className="">friends</div>
+        </div>
+    </div>
+  );
+}
+
+export default Profile;
