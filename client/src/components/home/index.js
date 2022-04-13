b0958d35ebeda7cb2508ed7d3d0d619983179ed7
elijahb22 <elibusick11@gmail.com>
2022-04-13 12:37:33 -0400

    import query posts to bring posts and give a loading indications that data is not there yet

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import { useQuery } from '@apollo/client';
+import { query_posts } from '../utils/queries';
 const home = () => {
+    const { loading, data } = useQuery(query_posts);
+    const posts = data?.posts || [];
+    console.log(posts)
     return(
         <main>
             <div className="flex-row justify-space-between">
                 <div className="">
-                    <div></div>
+                    {loading (
+                    <div>Loading</div>
+                    )}
                 </div>
             </div>
         </main>
