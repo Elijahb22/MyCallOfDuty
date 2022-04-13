b181b07381ab58c92b8861ba4facc00538f0ea8f
elijahb22 <elibusick11@gmail.com>
2022-04-13 09:41:30 -0400

    installed react router and added login route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Login from './components/login/index'
 const httpLink = createHttpLink({
   uri: 'http://localhost:3001/graphql',
 });
@@ -12,22 +14,12 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          <header></header>
+          <Route exact path="./components/login/index.js"></Route>
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
