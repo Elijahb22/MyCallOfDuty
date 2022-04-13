885e7ff05dd13c319df68d082a07104454e2a7b7
elijahb22 <elibusick11@gmail.com>
2022-04-13 10:20:17 -0400

    created footer component and exported it to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+import Header from './components/header';
 import Login from './components/login';
 import Profile from './components/profile';
 import Home from './components/home';
+import Footer from './components/footer';
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:3001/graphql',
@@ -19,10 +22,11 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div className="App">
-          <header></header>
+          <Header></Header>
           <Route exact path="/home" components={Home}></Route>
           <Route exact path="/login" component={Login}></Route>
           <Route exact path="/profile" component={Profile}></Route>
+          <Footer></Footer>
         </div>
       </Router>
     </ApolloProvider>
---
+++ client/src/components/footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="">
+      <div className="container">
+        &copy; Elijah Busick  
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
