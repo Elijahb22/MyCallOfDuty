c61d1c73df720af239b72f7e4803ad0372a84058
elijahb22 <elibusick11@gmail.com>
2022-04-14 17:03:03 -0400

    create a method to check if token is expired and retrive from local storage

--- client/src/components/utils/auth.js
+++ client/src/components/utils/auth.js
@@ -8,6 +8,21 @@ class AuthService {
         const token = this.getToken();
         return !!token && !this.isTokenExpired(token);
     }
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else {
+                return false;
+            }
+        }   catch (err) {
+                return false;
+            }
+    }
+    getToken() {
+        return localStorage.getItem('id_token');
+    }
 }
 
 export default new AuthService();
