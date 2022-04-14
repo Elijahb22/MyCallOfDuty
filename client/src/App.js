e07e8d6e4a74c311c6277bfc1b993d8a0c1c2da7
elijahb22 <elibusick11@gmail.com>
2022-04-14 17:46:17 -0400

    adde authentication to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Header from './components/header';
 import Login from './components/login';
@@ -14,9 +15,17 @@ import onePost from './components/onepost';
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
-
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 function App() {
