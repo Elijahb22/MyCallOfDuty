6b5f81996d05078a38a76d8110dd07aa8c4bef38
elijahb22 <elibusick11@gmail.com>
2022-04-14 13:47:19 -0400

    added query hook to load data

--- client/src/components/onepost/index.js
+++ client/src/components/onepost/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Comments from '../comments';
+import { useQuery } from '@apollo/client';
+import { Query_posts  } from '../utils/queries';
+import { useParams } from 'react-router-dom';
 
-const onePost = props => {
+const OnePost = props => {
+const { id: postId } = useParams();
+const { loading, data } = useQuery(Query_posts, {
+  variables: { id: postId }
+});
+const post = data?.post || {};
+if (loading) {
+  return <div>Loading...</div>;
+}
+
   return (
     <div>
       <div className="card">
@@ -9,17 +21,17 @@ const onePost = props => {
           <span className="">
             {post.username}
           </span>{' '}
-          Post {thought.createdAt}
+          Post {post.createdAt}
         </p>
         <div className="card-body">
           <p>{post.postText}</p>
         </div>
       </div>
       {post.commentCount > 0 && (
-        <Comments comments={thought.comments}/>
+        <Comments comments={post.comments}/>
       )}
     </div>
   );
 };
 
-export default onePost;
+export default OnePost;
