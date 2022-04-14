7e9aacaa99ce07abd29ed033d69ad7583d7c82a2
elijahb22 <elibusick11@gmail.com>
2022-04-14 01:41:02 -0400

    created function to update state on form input changes

--- client/src/components/signUp/index.js
+++ client/src/components/signUp/index.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Signup = () => {
+  const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+  const change = event => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  };
   const handleFormSubmit = async event => {
     event.preventDefault();
   };
@@ -11,9 +20,9 @@ const Signup = () => {
           <h4 className="">Sign Up</h4>
           <div className="card-body">
             <form onSubmit={handleFormSubmit}>
-              <input className="form-input" placeholder="Your username" name="username" type="username"id="username"/> 
-              <input className="form-input" placeholder="Your email" name="email" type="email" id="email"/>
-              <input className="form-input" placeholder="Password" name="password" type="password" id="password"/>
+              <input className="form-input" placeholder="Your username" name="username" type="username"id="username" value={formState.username} onChange={change}/> 
+              <input className="form-input" placeholder="Your email" name="email" type="email" id="email" value={formState.email} onChange={change}/>
+              <input className="form-input" placeholder="Password" name="password" type="password" id="password" value={formState.password} onChange={change}/>
               <button className="btn" type="submit">Submit</button>
             </form>
           </div>
