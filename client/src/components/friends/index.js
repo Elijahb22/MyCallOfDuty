03e39707bc6a59fdb35d612e8819b4649d29dc09
elijahb22 <elibusick11@gmail.com>
2022-04-14 15:01:09 -0400

    create frinds component

---
+++ client/src/components/friends/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const Friends = () => {
+  
+    return (
+      <div>
+        <h5>
+          friendCount
+        </h5>
+        <button>
+            Add Friend
+        </button>
+      </div>
+    )
+};
+  
+export default Friends;
