744f136b9ad4496733a8eb8c2e2ea8381768e68b
elijahb22 <elibusick11@gmail.com>
2022-04-14 12:14:23 -0400

    added comments component

---
+++ client/src/components/comments/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const comments = () => {
+    return (
+      <div className="card">
+        <div className="card-header">
+          <span className="">Comments</span>
+        </div>
+        <div className="card-body">
+        </div>
+      </div>
+    );
+};
+
+export default comments;
