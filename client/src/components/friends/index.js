832e6cef62ded19ec93d2f5aa405fb36c53e7689
elijahb22 <elibusick11@gmail.com>
2022-04-14 15:22:12 -0400

    added friend count

--- client/src/components/friends/index.js
+++ client/src/components/friends/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Friends = () => {
-  
+const Friends = ({ friendCount, username, friends }) => {
+    if (!friends || !friends.length) {
+        return <p className="">{username} add friends!</p>;
+    }
+    
     return (
       <div>
         <h5>
-          friendCount
+            {username}'s {friendCount} {friendCount === 1 ? 'friend' : 'friends'}
         </h5>
         <button>
             <Link to={`/profile/${friend.username}`}>{friend.username}</Link>
