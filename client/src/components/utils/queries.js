836579af6b2e18a4f2b8209672bc038f973f0226
elijahb22 <elibusick11@gmail.com>
2022-04-14 14:29:27 -0400

    create user query

--- client/src/components/utils/queries.js
+++ client/src/components/utils/queries.js
@@ -35,3 +35,24 @@ export const Query_post = gql`
     }
   }
 `;
+export const Query_user = gql`
+query user($username: String!) {
+  user(username: $username) {
+    _id
+    username
+    email
+    friendCount
+    friends {
+      _id
+      username
+    }
+    posts {
+      _id
+      postText
+      createdAt
+      commentCount
+    }
+  }
+}
+`;
+
