ff461c21caa0b64f5739aff89188687fbad4a346
elijahb22 <elibusick11@gmail.com>
2022-04-14 15:57:09 -0400

    created login mutation

---
+++ client/src/components/utils/mutations.js
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+export const Login_user= gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
