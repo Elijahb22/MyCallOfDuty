ad659ee16d8c5745d250488a5a8a40ee1429cf3f
elijahb22 <elibusick11@gmail.com>
2022-04-14 12:23:10 -0400

    added link from profile to comment

--- client/src/components/comments/index.js
+++ client/src/components/comments/index.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
-const comments = () => {
+
+const comments = (comments) => {
     return (
       <div className="card">
         <div className="card-header">
           <span className="">Comments</span>
         </div>
         <div className="card-body">
+            <Link to={`/profile/${comment.username}`} >
+                {comment.username} on {comment.createdAt}
+            </Link>
         </div>
       </div>
     );
