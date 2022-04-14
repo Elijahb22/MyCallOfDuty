01e250b3bf694c7a07b4a2e77e6769e510a9c299
elijahb22 <elibusick11@gmail.com>
2022-04-14 18:20:51 -0400

    add friend list

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Posts from '../posts';
-
+import Friends from '../friends';
 import { useQuery } from '@apollo/client';
 import { Query_posts, Query_mes } from '../utils/queries';
 import Auth from '../utils/auth';
@@ -8,7 +8,7 @@ const Home = () => {
     const { loading, data } = useQuery(Query_posts);
     const posts = data?.posts || [];
     const loggedIn = Auth.loggedIn();
-
+    const { data: userData } = useQuery(Query_mes);
     return(
         <main>
             <div className="flex-row justify-space-between">
@@ -19,6 +19,10 @@ const Home = () => {
                     <Posts posts={posts} title="What would you like to post?" />
                 )}
                 </div>
+                {loggedIn && userData ? (
+                <div className="col-12 col-lg-3 mb-3">
+                    <FriendList username={userData.me.username} friendCount={userData.me.friendCount} friends={userData.me.friends}/>
+                </div> ) : null}
             </div>
         </main>
     )
