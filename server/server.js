3b8ade748345d7439a686d07886855142c4780af
elijahb22 <elibusick11@gmail.com>
2022-04-14 20:10:11 -0400

    fix server

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../public/build')));
+  app.use(express.static(path.join(__dirname, '../public/index.html')));
 }
 
 app.get('*', (req, res) => {
