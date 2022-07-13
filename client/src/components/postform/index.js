47456e043b672b66e6ce73c4cc8d40c82058c1d2
elijahb22 <elibusick11@gmail.com>
2022-07-13 15:41:17 -0400

    created post form to create a post

---
+++ client/src/components/postform/index.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+
+import { useMutation } from '@apollo/client';
+import { Add_post } from '../../utils/mutations';
+import { Query_posts, Query_me } from '../../utils/queries';
+
+const postForm = () => {
+  const [postText, setText] = useState('');
+  const [characterCount, setCharacterCount] = useState(0);
+
+  const [addPost, { error }] = useMutation(Add_post, {
+    update(cache, { data: { addPost } }) {
+      try {
+        // update thought array's cache
+        // could potentially not exist yet, so wrap in a try/catch
+        const { posts } = cache.readQuery({ query: Query_posts });
+        cache.writeQuery({
+          query: Query_posts,
+          data: { posts: [addPost, ...posts] },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+
+      // update me object's cache
+      const { me } = cache.readQuery({ query: Query_me });
+      cache.writeQuery({
+        query: Query_me,
+        data: { me: { ...me, thoughts: [...me.posts, addPost] } },
+      });
+    },
+  });
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    if (event.target.value.length <= 280) {
+      setText(event.target.value);
+      setCharacterCount(event.target.value.length);
+    }
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await addPost({
+        variables: { postText },
+      });
+
+      // clear form value
+      setText('');
+      setCharacterCount(0);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div>
+      <p
+        className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}
+      >
+        Character Count: {characterCount}/280
+        {error && <span className="ml-2">Something went wrong...</span>}
+      </p>
+      <form
+        className="flex-row justify-center justify-space-between-md align-stretch"
+        onSubmit={handleFormSubmit}
+      >
+        <textarea
+          placeholder="Here's a new thought..."
+          value={ postText }
+          className="form-input col-12 col-md-9"
+          onChange={handleChange}
+        ></textarea>
+        <button className="btn col-12 col-md-3" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default postForm;
