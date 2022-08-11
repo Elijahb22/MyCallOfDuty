8b50afa1f9cbcd335acb2b2126340722e67e37f8
elijahb22 <elibusick11@gmail.com>
2022-08-11 12:41:14 -0400

    connected post form to home

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Posts from '../../components/posts';
 import Friends from '../../components/friends';
+import PostsForm from '../../components/postform'
+
 import { useQuery } from '@apollo/client';
 import { Query_posts, Query_mes } from '../../utils/queries';
 import Auth from '../../utils/auth';
+
 const Home = () => {
     const { loading, data } = useQuery(Query_posts);
     const posts = data?.posts || [];
@@ -12,6 +15,11 @@ const Home = () => {
     return(
         <main>
             <div className="flex-row justify-space-between">
+                {loggedIn && (
+                <div className="col-12 mb-3">
+                    <PostsForm />
+                </div>
+                )}
                 <div className={`${loggedIn && 'col-lg-8'}`}>
                 {loading ? (
                     <div>Loading...</div>
