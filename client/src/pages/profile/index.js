890d65114dcf936d06ba7ba3815f9e33d268730e
elijahb22 <elibusick11@gmail.com>
2022-10-06 12:16:04 -0400

    created line of data to save bio and rank text

--- client/src/pages/profile/index.js
+++ client/src/pages/profile/index.js
@@ -3,12 +3,15 @@ import Select from 'react-select';
 import { Redirect, useParams } from 'react-router-dom';
 import Friends from '../../components/friends';
 import Posts from '../../components/posts';
+import PostForm from '../../components/postform';
 import { useQuery, useMutation } from '@apollo/client';
 import { Query_user, Query_me } from '../../utils/queries';
 import Auth from '../../utils/auth';
 import { Add_friend } from '../../utils/mutations';
+import { useState } from 'react';
 
 const Profile = () => {
+var [selectText] = useState('')
 const [addFriend] = useMutation(Add_friend);
 const { username: userParam } = useParams();
 const { loading, data } = useQuery(userParam ? Query_user : Query_me, {
@@ -42,18 +45,24 @@ const rank = [
     { label: "Top 250", value: 8 },
     { label: " Warzone", value: 9 }
 ]
+const savedata = async (event) => {
+    selectText(event.target.value);
+}
+
 return (
     <div>
         <div className="">
             <h2 className="">{user.username}'s profile.</h2>
                 <div>
                     <label>Bio:</label>
-                    <input placeholder=''></input>
+                    <input placeholder='Tell people about your self!' onChange={savedata}></input>
                 </div>
                 <div>
                     <label>Rank or Warzone: </label>
-                    <Select placeholder="N/A" options={rank}></Select>
+                    <Select id='ranks' placeholder="N/A" options={rank} onChange={savedata}></Select>
+                    <button>submit</button>
                 </div>
+
             <button className="btn " onClick={ handleClick }>Add Friend</button>
         </div>
         <div className="flex-row justify-space-between">
@@ -62,6 +71,7 @@ return (
             </div>
             <div className=""><Friends username={user.username} friendCount={user.friendCount} friends={user.friends}/></div>
         </div>
+        <div className="mb-3">{!userParam && <PostForm />}</div>
     </div>
   );
 }
